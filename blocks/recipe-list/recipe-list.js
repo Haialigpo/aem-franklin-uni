45677a26187252af2a3fcf5db4bce977d7acbd7e
Ankur <ankurvishnoi123@gmail.com>
2024-05-17 12:29:24 +0530

    lint

--- blocks/recipe-list/recipe-list.js
+++ blocks/recipe-list/recipe-list.js
@@ -1,25 +1,25 @@
 // Function to initialize the component and make the HTTP request
 function initProductListComponents() {
   // Select all div elements with the class 'recipe-list-container'
-  const divs = document.querySelectorAll(".recipe-list-container");
+  const divs = document.querySelectorAll('.recipe-list-container');
 
   // Iterate over each div and perform the necessary operations
   divs.forEach((div) => {
     function createProductList(products) {
-      let productListHTML = "";
+      let productListHTML = '';
       products.forEach((product) => {
         productListHTML += `
-                            <div class="recipe-item">
-                               <a href="${`pdp?product=${product.sku}`}" aria-label="View details for ${product.name}">
-                                <img src="${`https://assets.unileversolutions.com/v1/${product.thumbnail.url}.png?im=AspectCrop=(400,400);Resize=(400,400)`}" alt="${product.thumbnail.label}">
+                            <div class='recipe-item'>
+                               <a href='${`pdp?product=${product.sku}`}' aria-label='View details for ${product.name}'>
+                                <img src='${`https://assets.unileversolutions.com/v1/${product.thumbnail.url}.png?im=AspectCrop=(400,400);Resize=(400,400)`}' alt='${product.thumbnail.label}'>
                                  <h2>${product.name}</h2>
-                                <div class="product-description">${product.description.html}</div>
+                                <div class='product-description'>${product.description.html}</div>
                                 </a>
                             </div>
                         `;
       });
 
-      div.querySelector(".recipe-list").innerHTML += productListHTML;
+      div.querySelector('.recipe-list').innerHTML += productListHTML;
     }
 
     const { size, apiurl, store } = div.dataset;
@@ -28,13 +28,13 @@ function initProductListComponents() {
       const headers = {
         size,
         store,
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       };
       //  const loadmoreButtonText = div.dataset.loadmorebutton;
 
       const query = `
           query {
-            products(currentPage: ${page}, pageSize: ${size}, filter: {category_uid: {eq: "{\\"sort_by\\":\\"relevance\\"}"}}, sort: {price: ASC}) {
+            products(currentPage: ${page}, pageSize: ${size}, filter: {category_uid: {eq: '{\\'sort_by\\':\\'relevance\\'}'}}, sort: {price: ASC}) {
               total_count
               items {
                 __typename
@@ -78,7 +78,7 @@ function initProductListComponents() {
           }`;
 
       fetch(apiurl, {
-        method: "POST",
+        method: 'POST',
         headers,
         body: JSON.stringify({ query }),
       })
@@ -88,7 +88,7 @@ function initProductListComponents() {
           createProductList(data.data.products.items);
         })
         .catch((error) => {
-          console.error("Error:", error);
+          console.error('Error:', error);
         });
     };
     fetchAndDisplayProducts();
