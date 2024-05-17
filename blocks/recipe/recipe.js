f707ffd6b84436d167f671dd1572dfdf500c9daf
Ankur <ankurvishnoi123@gmail.com>
2024-05-17 14:15:48 +0530

    recipe

---
+++ blocks/recipe/recipe.js
@@ -0,0 +1,88 @@
+// Function to initialize the component and make the HTTP request
+function createrecipeHTML(recipe, div) {
+    let recipeHTML = '';
+    recipeHTML += `
+    <div class="recipe-info">
+          <div class="left-container">
+           <img src="${`${recipe.recipeData.image[0].default}?im=AspectCrop=(400,400);Resize=(400,400)`}" alt="${recipe.image.label}">
+          </div>
+          <div class="right-container">
+          <h1>${recipe.recipeData.name}</h1>
+          </div>
+          </div>
+      
+      `;
+  
+    div.querySelector('.recipe-wrapper').innerHTML += recipeHTML;
+  }
+  function fetchrecipeDetails(sku) {
+    const div = document.querySelector('.recipe-container');
+    const { apiurl, store } = div.dataset;
+  
+    const headers = {
+      store,
+      'Content-Type': 'application/json',
+    };
+    //  const loadmoreButtonText = div.dataset.loadmorebutton;
+  
+    const query = `{
+        recipes(filter: { recipeID: { eq: "104773" } }) {
+            total_Count
+            items {
+                recipeData {
+                    recipeID
+                    recipeName
+                    name
+                    image {
+                        default
+                    }
+                    newImage {
+                        default {
+                            url
+                            title
+                            caption
+                        }
+                        dish {
+                            url
+                            title
+                            caption
+                        }
+                        principle {
+                            url
+                            title
+                            caption
+                        }
+                        main_ingredient {
+                            url
+                            title
+                            caption
+                        }
+                    }
+                }
+            }
+        }
+    }`;
+  
+    fetch(apiurl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ query }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        createrecipeHTML(data.data.recipes.items[0], div);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  function initrecipeComponents() {
+    const url = window.location.href;
+    const urlObj = new URL(url);
+    const params = new URLSearchParams(urlObj.search);
+    const sku = params.get('recipe');
+    fetchrecipeDetails(sku);
+  }
+  
+  initrecipeComponents();
+  
