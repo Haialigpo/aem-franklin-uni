4d6bab1abe14c9977684964e96bbc090648e3b51
Peeyush Agrawal <peeyush.ag0212@gmail.com>
2024-05-23 11:41:25 +0530

    live chat css

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,9 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM, loadScript } from './aem.js';
+import { sampleRUM, loadScript, loadCSS } from './aem.js';
 // eslint-disable-next-line import/no-cycle
 import { getEnvType } from './scripts.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
-
 
 // add more delayed functionality here
 async function loadAdobeLaunch() {
@@ -13,7 +12,30 @@ async function loadAdobeLaunch() {
     preview: 'https://assets.adobedtm.com/e6bd1902389a/e558d14ace7f/launch-8fb839a92d58-development.min.js',
     live: 'https://assets.adobedtm.com/e6bd1902389a/e558d14ace7f/launch-8fb839a92d58-development.min.js',
   };
-  await loadScript(adobeotmSrc[getEnvType()]);
+  await loadScript(adobeotmSrc[getEnvType()], {});
 }
 
 await loadAdobeLaunch();
+
+async function loadLiveChat() {
+  const liveChatJsSrc = {
+    dev: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot.js?brand=knorr&locale=en_GB',
+    preview: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot.js?brand=knorr&locale=en_GB',
+    live: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot.js?brand=knorr&locale=en_GB',
+  };
+  const liveChatCssSrc = {
+    dev: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/BotChat.css?brand=knorr&locale=en_GB',
+    preview: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/BotChat.css?brand=knorr&locale=en_GB',
+    live: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/BotChat.css?brand=knorr&locale=en_GB',
+  };
+  const liveChatSkinCssSrc = {
+    dev: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot-Skin.css?brand=knorr&locale=en_GB',
+    preview: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot-Skin.css?brand=knorr&locale=en_GB',
+    live: 'https://azcb-ne-pas-cdnprod.azureedge.net/knorruk105/aemtemplate/Bot-Skin.css?brand=knorr&locale=en_GB',
+  };
+  await loadScript(liveChatJsSrc[getEnvType()], {});
+  await loadCSS(liveChatCssSrc[getEnvType()]);
+  await loadCSS(liveChatSkinCssSrc[getEnvType()]);
+}
+
+await loadLiveChat();
